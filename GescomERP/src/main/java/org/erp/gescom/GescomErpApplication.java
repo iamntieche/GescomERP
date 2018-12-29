c5e940077ac77801305c6ce930b769513a5da625
Ntieche Mfoum Issofa <youssouf.tiefa@gmail.com>
2018-12-29 07:35:26 +0100

    Initialisation

---
+++ GescomERP/src/main/java/org/erp/gescom/GescomErpApplication.java
@@ -0,0 +1,14 @@
+package org.erp.gescom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GescomErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GescomErpApplication.class, args);
+	}
+
+}
+
